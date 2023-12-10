d5e99af4eadaea7221d1a1b56b7b2cb6220f8923
Thafer Shahin <thafer.shahin@idcman.ae>
2023-12-11 00:42:06 +0400

    Initial Commit

---
+++ Great Sports/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Great Sports
+//
+//  Created by Thafer Shahin on 11/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Great Sports/Great_SportsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Great_SportsApp.swift
+//  Great Sports
+//
+//  Created by Thafer Shahin on 11/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct Great_SportsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
